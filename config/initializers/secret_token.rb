0a55b256620040aa00223578bcd624e093fd4a66
iGiveMore <Matt@igivemore.org>
2013-04-27 18:41:29 -0400

    all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweet4tweet::Application.config.secret_token = '09172fa55ea46aa07d840c9737811f5d368b999d296ab96322370f5072d7b1b7ea2a25d027fb09a8b0c6eb6e89cee4ead499ec9abd336152996aebde8c42173a'
